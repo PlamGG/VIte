9160400697aa6db6e4bc36435c4876f459af8d78
kittikhun tan <kittikhunforwork@gmail.com>
2024-06-21 03:47:23 +0700

    sadssasds

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'VIte',
+  base: '/VIte/',  // ตรงนี้ต้องตรงกับชื่อที่คุณตั้งไว้ใน GitHub Pages
   plugins: [react()],
-})
+});
