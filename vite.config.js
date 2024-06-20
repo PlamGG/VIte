e49f7706448c9211f728622b7d73e8c254bcb3c4
DuckerMAstor <145746972+PlamGG@users.noreply.github.com>
2024-06-21 04:02:36 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/VIte/',  // ตรงนี้ต้องตรงกับชื่อที่คุณตั้งไว้ใน GitHub Pages
+  base: '/VIte/',  
   plugins: [react()],
 });
